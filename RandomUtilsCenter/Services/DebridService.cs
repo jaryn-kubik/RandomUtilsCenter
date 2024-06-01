a9d7364de41ef3d9fa55bfa440452adca3057bd8
Jaroslav Kubík <kubik.jaroslav@outlook.cz>
2024-06-01 22:08:12 +0200

    net8

--- RandomUtilsCenter/Services/DebridService.cs
+++ RandomUtilsCenter/Services/DebridService.cs
@@ -7,110 +7,130 @@ using System.Threading.Tasks;
 
 namespace RandomUtilsCenter.Services
 {
-	public class DebridService
-	{
-		private readonly ConfigService _config;
-		private readonly TimerSync _sync;
-		private readonly SemaphoreSlim _clientSync = new(1, 1);
-		private RdNetClient _client;
+    public class DebridService
+    {
+        private readonly ConfigService _config;
+        private readonly TimerSync _sync;
+        private readonly SemaphoreSlim _clientSync = new(1, 1);
+        private RdNetClient _client;
 
-		public DebridService(ConfigService config)
-		{
-			_config = config;
-			_sync = new(TimeSpan.FromSeconds(5), ReloadAsync);
-		}
+        public DebridService(ConfigService config)
+        {
+            _config = config;
+            _sync = new(TimeSpan.FromSeconds(5), ReloadAsync);
+        }
 
-		private bool InitClient()
-		{
-			if (_client == null && string.IsNullOrWhiteSpace(_config.RealDebridApiKey))
-			{
-				return false;
-			}
-			_client = new RdNetClient();
-			_client.UseApiAuthentication(_config.RealDebridApiKey);
-			return true;
-		}
+        private bool InitClient()
+        {
+            if (_client == null && string.IsNullOrWhiteSpace(_config.RealDebridApiKey))
+            {
+                return false;
+            }
+            _client = new RdNetClient();
+            _client.UseApiAuthentication(_config.RealDebridApiKey);
+            return true;
+        }
 
-		public IList<Torrent> Items { get; private set; } = Array.Empty<Torrent>();
-		public event Action Changed;
+        public IList<Torrent> Items { get; private set; } = Array.Empty<Torrent>();
+        public event Action Changed;
 
-		public IEnumerable<Torrent> GetTorrents(string hash)
-		{
-			return Items.Where(x => string.Equals(x.Hash, hash, StringComparison.OrdinalIgnoreCase));
-		}
+        public IEnumerable<Torrent> GetTorrents(string hash)
+        {
+            return Items.Where(x => string.Equals(x.Hash, hash, StringComparison.OrdinalIgnoreCase));
+        }
 
-		public Torrent GetTorrent(string hash)
-		{
-			return GetTorrents(hash).FirstOrDefault();
-		}
+        public Torrent GetTorrent(string hash)
+        {
+            return GetTorrents(hash).FirstOrDefault();
+        }
 
-		public async Task ReloadAsync()
-		{
-			if (InitClient())
-			{
-				await _sync.RunAsync(async () =>
-				{
-					Items = await _client.Torrents.GetAsync(null, 100);
-					if (Items.Count == 0 || Items.All(x => x.Status == "downloaded"))
-					{
-						_sync.Stop();
-					}
-					else
-					{
-						_sync.Start();
-					}
-				});
-				Changed?.Invoke();
-			}
-		}
+        public async Task ReloadAsync()
+        {
+            if (InitClient())
+            {
+                await _sync.RunAsync(async () =>
+                {
+                    Items = await _client.Torrents.GetAsync(null, 100);
+                    foreach (var torrent in Items)
+                    {
+                        await SelectFilesAsync(torrent);
+                    }
 
-		public async Task AddMagnetAsync(string hash, string magnet)
-		{
-			if (InitClient())
-			{
-				await ReloadAsync();
-				if (!GetTorrents(hash).Any())
-				{
-					var result = await _client.Torrents.AddMagnetAsync(magnet);
-					var torrent = await _client.Torrents.GetInfoAsync(result.Id);
-					if (torrent.Status == "waiting_files_selection")
-					{
-						var files = torrent.Files.Where(x =>
-							x.Bytes > 1024 &&
-							!x.Path.EndsWith(".txt", StringComparison.OrdinalIgnoreCase) &&
-							!x.Path.EndsWith(".nfo", StringComparison.OrdinalIgnoreCase) &&
-							!x.Path.EndsWith(".srt", StringComparison.OrdinalIgnoreCase) &&
-							!x.Path.EndsWith(".sub", StringComparison.OrdinalIgnoreCase) &&
-							!x.Path.EndsWith(".ass", StringComparison.OrdinalIgnoreCase)
-						);
-						await _client.Torrents.SelectFilesAsync(result.Id, files.Select(x => x.Id.ToString()).ToArray());
-					}
-				}
-				await ReloadAsync();
-			}
-		}
+                    if (Items.Count == 0 || Items.All(x => x.Status == "downloaded"))
+                    {
+                        _sync.Stop();
+                    }
+                    else
+                    {
+                        _sync.Start();
+                    }
+                });
+                Changed?.Invoke();
+            }
+        }
 
-		public async Task DeleteAsync(string hash)
-		{
-			if (InitClient())
-			{
-				foreach (var torrent in GetTorrents(hash).ToArray())
-				{
-					await _client.Torrents.DeleteAsync(torrent.Id);
-				}
-				await ReloadAsync();
-			}
-		}
+        public async Task AddMagnetAsync(string hash, string magnet)
+        {
+            if (InitClient())
+            {
+                await ReloadAsync();
+                if (!GetTorrents(hash).Any())
+                {
+                    var result = await _client.Torrents.AddMagnetAsync(magnet);
+                    var torrent = await _client.Torrents.GetInfoAsync(result.Id);
+                    await SelectFilesAsync(torrent);
+                }
+                await ReloadAsync();
+            }
+        }
 
-		public async Task<UnrestrictLink> UnrestrictAsync(string link)
-		{
-			if (InitClient())
-			{
-				await _clientSync.WaitAsync();
-				try { return await _client.Unrestrict.LinkAsync(link); }
-				finally { _clientSync.Release(); }
-			}
-			return new UnrestrictLink();
-		}
-	}
+        public async Task DeleteAsync(string hash)
+        {
+            if (InitClient())
+            {
+                foreach (var torrent in GetTorrents(hash).ToArray())
+                {
+                    await _client.Torrents.DeleteAsync(torrent.Id);
+                }
+                await ReloadAsync();
+            }
+        }
+
+        public async Task<UnrestrictLink> UnrestrictAsync(string link)
+        {
+            if (InitClient())
+            {
+                await _clientSync.WaitAsync();
+                try { return await _client.Unrestrict.LinkAsync(link); }
+                finally { _clientSync.Release(); }
+            }
+            return new UnrestrictLink();
+        }
+
+        private async Task SelectFilesAsync(Torrent torrent)
+        {
+            if (torrent.Status == "waiting_files_selection")
+            {
+                if (torrent.Files is null)
+                {
+                    torrent = await _client.Torrents.GetInfoAsync(torrent.Id);
+                }
+                var files = torrent.Files
+                    ?.Where(x =>
+                        x.Bytes > 1024 &&
+                       !x.Path.EndsWith(".txt", StringComparison.OrdinalIgnoreCase) &&
+                       !x.Path.EndsWith(".nfo", StringComparison.OrdinalIgnoreCase) &&
+                       !x.Path.EndsWith(".srt", StringComparison.OrdinalIgnoreCase) &&
+                       !x.Path.EndsWith(".sub", StringComparison.OrdinalIgnoreCase) &&
+                       !x.Path.EndsWith(".ass", StringComparison.OrdinalIgnoreCase)
+                    )
+                    ?.Select(x => x?.Id.ToString())
+                    ?.ToArray();
+                if (files?.Length > 0)
+                {
+                    await _client.Torrents.SelectFilesAsync(torrent.Id, files);
+                }
+            }
+        }
+    }
 }
--- RandomUtilsCenter/Services/YtDlpService.cs
+++ RandomUtilsCenter/Services/YtDlpService.cs
@@ -1,28 +1,27 @@
 ﻿using Microsoft.Extensions.Logging;
 using Python.Runtime;
 using System;
-using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace RandomUtilsCenter.Services
 {
-	public class YtDlpService
-	{
-		private readonly ILogger<YtDlpService> _logger;
-		private readonly CircuitsService _circuitsService;
-		private IntPtr _threadState;
+    public class YtDlpService
+    {
+        private readonly ILogger<YtDlpService> _logger;
+        private readonly CircuitsService _circuitsService;
+        private readonly IntPtr _threadState;
 
-		public YtDlpService(ILogger<YtDlpService> logger, CircuitsService circuitsService, ClipboardService clipboard)
-		{
-			_logger = logger;
-			_circuitsService = circuitsService;
-			clipboard.Register(OnClipboardAsync);
-		}
+        public YtDlpService(ILogger<YtDlpService> logger, CircuitsService circuitsService, ClipboardService clipboard)
+        {
+            _logger = logger;
+            _circuitsService = circuitsService;
+            clipboard.Register(OnClipboardAsync);
+        }
 
-		private Task OnClipboardAsync(string text)
-		{
-			if (_circuitsService.Any && text.StartsWith("https://"))
+        private Task OnClipboardAsync(string text)
+        {
+            /*if (_circuitsService.Any && text.StartsWith("https://"))
 			{
 				var extractor = GetExtractor(text);
 				if (extractor != "Generic")
@@ -30,49 +29,49 @@ namespace RandomUtilsCenter.Services
 					Utils.ShowToast(extractor, text);
 					Download(text);
 				}
-			}
-			return Task.CompletedTask;
-		}
+			}*/
+            return Task.CompletedTask;
+        }
 
-		public async Task UpdateAsync(CancellationToken cancellationToken)
-		{
-			var startInfo = new ProcessStartInfo("python.exe", "-m pip install -U yt-dlp") { CreateNoWindow = true };
-			await Process.Start(startInfo).WaitForExitAsync(cancellationToken);
-		}
+        public async Task UpdateAsync(CancellationToken cancellationToken)
+        {
+            /*var startInfo = new ProcessStartInfo("python.exe", "-m pip install -U yt-dlp") { CreateNoWindow = true };
+			await Process.Start(startInfo).WaitForExitAsync(cancellationToken);*/
+        }
 
-		public void Initialize()
-		{
-			Runtime.PythonDLL = "python310.dll";
+        public void Initialize()
+        {
+            /*Runtime.PythonDLL = "python310.dll";
 			PythonEngine.Initialize();
 			_threadState = PythonEngine.BeginAllowThreads();
 
-			Call<bool>(x => x.Init(new YtDlpLogger(_logger).ToPython()));
-		}
+			Call<bool>(x => x.Init(new YtDlpLogger(_logger).ToPython()));*/
+        }
 
-		public void Shutdown()
-		{
-			PythonEngine.EndAllowThreads(_threadState);
-			PythonEngine.Shutdown();
-		}
+        public void Shutdown()
+        {
+            /*PythonEngine.EndAllowThreads(_threadState);
+			PythonEngine.Shutdown();*/
+        }
 
-		private static T Call<T>(Func<dynamic, T> action)
-		{
-			using (Py.GIL())
-			{
-				try
-				{
-					dynamic ytDlp = Py.Import("yt-dlp-wrapper");
-					return action(ytDlp);
-				}
-				catch (Exception ex)
-				{
-					Utils.ShowMessage("Error - YtDlpService", $"{ex}");
-					throw;
-				}
-			}
-		}
+        private static T Call<T>(Func<dynamic, T> action)
+        {
+            using (Py.GIL())
+            {
+                try
+                {
+                    dynamic ytDlp = Py.Import("yt-dlp-wrapper");
+                    return action(ytDlp);
+                }
+                catch (Exception ex)
+                {
+                    Utils.ShowMessage("Error - YtDlpService", $"{ex}");
+                    throw;
+                }
+            }
+        }
 
-		private static string GetExtractor(string url) => Call<string>(x => x.GetExtractor(url));
-		private static int Download(string url) => Call<int>(x => x.Download(url));
-	}
+        private static string GetExtractor(string url) => Call<string>(x => x.GetExtractor(url));
+        private static int Download(string url) => Call<int>(x => x.Download(url));
+    }
 }
